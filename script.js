94060e048ba62920444e1740553d962237e5e5f0
vishnugamini <gamini.vishnu@gmail.com>
2023-01-03 14:09:18 +0530

    refactored code and updated styles

--- script.js
+++ script.js
@@ -15,9 +15,12 @@ var playerOne = document.querySelector('.player--1');
 
 var hasWon = false;
 
-document.querySelector('.btn--roll').addEventListener('click', rollDice);
+dice.classList.add('hidden');
 
-document.querySelector('.btn--new').addEventListener('click',function(){
+document.querySelector('.btn--roll').addEventListener('click', rollDice);
+document.querySelector('.btn--hold').addEventListener('click', MainScoreUpdate);
+
+document.querySelector('.btn--new').addEventListener('click', function () {
   playerOneActivity = true;
   playerTwoActivity = false;
   activePlayer();
@@ -27,48 +30,43 @@ document.querySelector('.btn--new').addEventListener('click',function(){
   playerTwoCurrentScore.textContent = '0';
   document.querySelector('.dice').src = '';
   hasWon = false;
-})
+  playerZero.classList.remove('player--loser');
+  playerOne.classList.remove('player--loser');
+  playerZero.classList.remove('player--winner');
+  playerOne.classList.remove('player--winner');
+});
 
 function rollDice() {
-  if(hasWon === false){
-    winner();
+  dice.classList.remove('hidden');
+  if (hasWon === true) {
+  } else if (hasWon === false) {
     var value = Math.trunc(Math.random() * 6) + 1;
-    const arr = ['dice-1.png','dice-2.png','dice-3.png','dice-4.png','dice-5.png','dice-6.png']
-    document.querySelector('.dice').src = arr[value - 1]
+    console.log(`dice-${value}.png`);
+    dice.src = `dice-${value}.png`;
     addscores(value);
-  } 
-  if (hasWon === true){
-
-  } 
+  }
 }
 
 function addscores(diceValue) {
-  document.querySelector('.btn--hold').addEventListener('click',MainScoreUpdate);
-  
-  if (playerOneActivity === true && playerTwoActivity === false){
+  if (playerOneActivity === true && playerTwoActivity === false) {
     if (diceValue != 1) {
-      var playerOneNum = String( Number(playerOneCurrentScore.textContent) + diceValue );
+      var playerOneNum = String(
+        Number(playerOneCurrentScore.textContent) + diceValue
+      );
       playerOneCurrentScore.textContent = playerOneNum;
-    } 
-    
-    else if (diceValue == 1){
+    } else if (diceValue == 1) {
       playerOneCurrentScore.textContent = '0';
       playerOneActivity = false;
       playerTwoActivity = true;
       activePlayer();
     }
-  }
-
-  else if (playerTwoActivity === true && playerOneActivity === false){
-
+  } else if (playerTwoActivity === true && playerOneActivity === false) {
     if (diceValue != 1) {
       var playerTwoNum = String(
         Number(playerTwoCurrentScore.textContent) + diceValue
       );
       playerTwoCurrentScore.textContent = playerTwoNum;
-    } 
-    
-    else if (diceValue == 1){
+    } else if (diceValue == 1) {
       playerTwoCurrentScore.textContent = '0';
       playerTwoActivity = false;
       playerOneActivity = true;
@@ -78,52 +76,51 @@ function addscores(diceValue) {
 }
 
 function MainScoreUpdate() {
-  if(hasWon === false){
-    winner();
-  }  
-
-  if(hasWon === true){
-
-  }
-  
-  else if(playerOneActivity === true){
-    var playerOneValue = Number(playerOneCurrentScore.textContent) + Number(playerOneScore.textContent);
+  if (hasWon === true) {
+  } else if (playerOneActivity === true) {
+    var playerOneValue =
+      Number(playerOneCurrentScore.textContent) +
+      Number(playerOneScore.textContent);
     playerOneScore.textContent = String(playerOneValue);
     playerOneCurrentScore.textContent = '0';
     playerOneActivity = false;
     playerTwoActivity = true;
     activePlayer();
-  }
-  else if (playerTwoActivity === true){
-    var playerTwoValue = Number(playerTwoCurrentScore.textContent) + Number(playerTwoScore.textContent);
+  } else if (playerTwoActivity === true) {
+    var playerTwoValue =
+      Number(playerTwoCurrentScore.textContent) +
+      Number(playerTwoScore.textContent);
     playerTwoScore.textContent = String(playerTwoValue);
     playerTwoCurrentScore.textContent = '0';
     playerTwoActivity = false;
     playerOneActivity = true;
     activePlayer();
-  }  
+  }
+  winner();
 }
 
-function activePlayer(){
-  if(playerOneActivity === true){
+function activePlayer() {
+  if (playerOneActivity === true) {
     playerZero.classList.add('player--active');
     playerOne.classList.remove('player--active');
-  }
-  else if(playerTwoActivity === true){
+  } else if (playerTwoActivity === true) {
     playerZero.classList.remove('player--active');
     playerOne.classList.add('player--active');
   }
 }
 
-function winner(){
-  if(hasWon === false){
-    if(Number(playerOneScore.textContent) >= 50){
-      hasWon = true;
-      console.log('player one has won');
-    }
-    else if (Number(playerTwoScore.textContent) >= 50){
-      hasWon = true;
-      console.log('player two has won');
-    }
-  } 
+function winner() {
+  if (Number(playerOneScore.textContent) >= 50) {
+    playerZero.classList.remove('player--active');
+    playerZero.classList.add('player--winner');
+    playerOne.classList.add('player--loser');
+    hasWon = true;
+    console.log('player one has won');
+  } else if (Number(playerTwoScore.textContent) >= 50) {
+    playerZero.classList.add('player--loser');
+    playerOne.classList.remove('player--active');
+    playerOne.classList.add('player--winner');
+    hasWon = true;
+    console.log('player two has won');
+  }
 }
